b21bf98e3c86fb76a55ec66125f845fde3b085fe
Rahmad Ade Akbar <rahmadadeakbar@gmail.com>
2020-08-27 21:34:28 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<script>
+    var data={
+        'a':'data1',
+        'b': 'data2'
+    };
+    data['c'] = 'data3';
+
+    var data2={
+
+    }
+    alert(JSON.stringify(data));
+</script>
+
+<?php 
+    $data = ['a' =>'data1', 'b' => 'data2'];
+    $data['c']= 'data3';
+
+?>
+<?php
+    $a=1;
+    $b=2;
+    
+?>
